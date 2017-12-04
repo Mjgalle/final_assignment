10f8d225245d6adb12e647db2cb0301bf1b3c5b6
Maria Gallegos <mjgallegosv05@gmail.com>
2017-12-04 14:04:31 -0500

    added react router dom twice

---
+++ memory-game/src/Memory.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Memory extends Component {
+  render() {
+    return (
+      <div className="container jumbotron">
+          <h1 className="App-title">Welcome to My Memory Game</h1>
+          <p>Heres some information about this game.. blah blah 
+			reay to play? </p>
+      </div>
+    );
+  }
+}
+
+export default Memory;
--- memory-game/src/index.js
+++ memory-game/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Memory from './Memory';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path='/' component={Memory} />
+        </Switch>
+    </Router>
+
+, document.getElementById('root')); 
+
